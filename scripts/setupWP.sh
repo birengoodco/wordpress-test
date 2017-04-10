cf992e62b08ddc5054078e99cdbb392c5bc82402
Biren Shah <biren@good.co>
2017-04-10 11:43:46 -0400

    Updates to test CodeDeploy

--- scripts/setupWP.sh
+++ scripts/setupWP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create MySQL DB for WordPress
-mysql -e 'CREATE DATABASE wordpressdb;' -u root;
-mysql -e "GRANT ALL PRIVILEGES ON wordpressdb.* TO 'wordpressdb'@'localhost' IDENTIFIED BY 'passw0rd';" -u root;
-mysql -e 'FLUSH PRIVILEGES;' -u root;
+#mysql -e 'CREATE DATABASE wordpressdb;' -u root;
+#mysql -e "GRANT ALL PRIVILEGES ON wordpressdb.* TO 'wordpressdb'@'localhost' IDENTIFIED BY 'passw0rd';" -u root;
+#mysql -e 'FLUSH PRIVILEGES;' -u root;
 
 # Use cURL to fetch WP-CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -18,13 +18,13 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 sudo wp core download --locale=en_US --allow-root --path=/var/www/html/wordpress
 
 # Generate `wp-config.php` file
-sudo wp core config --allow-root --dbhost=127.0.0.1 --dbname=wordpressdb --dbuser=wordpressdb --dbpass=passw0rd --path=/var/www/html/wordpress
+sudo wp core config --allow-root --dbhost=wordpressdb-cluster.cluster-c0frm1tm3eri.eu-west-1.rds.amazonaws.com --dbname=wordpressdb --dbuser=wordpressdb --dbpass=passw0rd --path=/var/www/html/wordpress
 
 # Install WordPress
-sudo wp core install --allow-root --admin_name=wpadmin --admin_password=rY9D2K9^F69y --admin_email=biren@good.co --url=http://wp-test --title=Wo
+sudo wp core install --allow-root --admin_name=wpadmin --admin_password=passw0rd --admin_email=biren@good.co --url=http://wp-test --title=WordPress --path=/var/www/html/wordpress
 
 # Update Site URL since it was installed to a sub-directory
-sudo wp option update --allow-root siteurl http://wp-test/wordpress/ --path=/var/www/html/wordpress
+#sudo wp option update --allow-root siteurl http://wp-test/wordpress/ --path=/var/www/html/wordpress
 
 # Restart the Apache server
 sudo service apache2 restart
